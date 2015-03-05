f0f3cc6d36c13fd9c61bfdce7ecca131f24367cb
bergerinn <and.bergsson@gmail.com>
2015-03-05 21:51:13 +0000

    Creating the classes

--- src/HotelSearchEngine/HotelManagement.java
+++ src/HotelSearchEngine/HotelManagement.java
@@ -1,8 +1,50 @@
 package HotelSearchEngine;
 
+import java.util.*;
 /**
  * Created by Andri on 05/03/15.
  */
 public class HotelManagement {
-    //Hallóhalló
+
+    public HotelManagement() {
+    }
+
+    /**
+     *
+     */
+    private void searchQuery;
+
+    /**
+     *
+     */
+    private void book;
+
+    /**
+     *
+     */
+    private void hotelStays;
+
+
+
+
+    /**
+     * @param searchObject
+     * @return
+     */
+    public HotelStay searchResults(SearchQuery searchObject) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param theRoomToBook
+     * @param book
+     * @return
+     */
+    public void bookRoom(HotelStay theRoomToBook, Booking book) {
+        // TODO implement here
+        return null;
+    }
+
+
 }
