6b39c1344c00b6f221be5bfb6814aaa8f51633ce
bergerinn <and.bergsson@gmail.com>
2015-03-06 10:36:44 +0000

    Creating the classes

--- src/HotelSearchEngine/HotelStay.java
+++ src/HotelSearchEngine/HotelStay.java
@@ -1,7 +1,194 @@
 package HotelSearchEngine;
 
+
+import java.util.*;
+
 /**
- * Created by Andri on 05/03/15.
+ *
  */
 public class HotelStay {
+
+    /**
+     *
+     */
+    public HotelStay() {
+    }
+
+    /**
+     *
+     */
+    private void hotelName;
+
+    /**
+     *
+     */
+    private void location;
+
+    /**
+     *
+     */
+    private void rating;
+
+    /**
+     *
+     */
+    private void wiFi;
+
+    /**
+     *
+     */
+    private void rooms;
+
+    /**
+     *
+     */
+    private void checkInTime;
+
+    /**
+     *
+     */
+    private void checkOutTime;
+
+    /**
+     *
+     */
+    private void totalPrice;
+
+
+    /**
+     * @param name
+     * @return
+     */
+    public void setHotelName(String name) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param loc
+     * @return
+     */
+    public void setLocation(String loc) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param rating
+     * @return
+     */
+    public void setRating(int rating) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param yesOrNo
+     * @return
+     */
+    public void setWiFi(boolean yesOrNo) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param room
+     * @return
+     */
+    public void setRoom(Room room) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param time
+     * @return
+     */
+    public void setCheckInTime(Date time) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param time
+     * @return
+     */
+    public void setCheckOutTime(Date time) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param price
+     * @return
+     */
+    public void setTotalPrice(int price) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getHotelName() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public String getLocation() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getRating() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public boolean getWiFi() {
+        // TODO implement here
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    public Room getRooms() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public Date getCheckInTime() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public Date getCheckOutTime() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public int getTotalPrice() {
+        // TODO implement here
+        return 0;
+    }
+
 }
