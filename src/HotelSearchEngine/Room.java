11ef1309ce6193ac0560fc329af4193eaf4bc792
bergerinn <and.bergsson@gmail.com>
2015-03-06 10:40:42 +0000

    Creating the classes

--- src/HotelSearchEngine/Room.java
+++ src/HotelSearchEngine/Room.java
@@ -1,7 +1,55 @@
 package HotelSearchEngine;
 
+import java.util.*;
+
 /**
  * Created by Andri on 05/03/15.
  */
+
+
 public class Room {
+
+    /**
+     *
+     */
+    public Room() {
+    }
+
+    /**
+     *
+     */
+    private void typeOfRoom;
+
+    /**
+     *
+     */
+    private void roomPrice;
+
+    /**
+     *
+     */
+    private void isOccupied;
+
+    /**
+     *
+     */
+    private void roomId;
+
+    /**
+     * @param isOccupied
+     * @return
+     */
+    public void setOccupyStatus(boolean isOccupied) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public int getRoomId() {
+        // TODO implement here
+        return 0;
+    }
+
 }
