b568445f7a91e692ed6690a4ba87e0d5660797d4
bergerinn <and.bergsson@gmail.com>
2015-03-06 13:47:44 +0000

    Configuring tests

--- src/Tests/HotelManagementTest.java
+++ src/Tests/HotelManagementTest.java
@@ -1,20 +1,36 @@
 package Tests;
 
+import HotelSearchEngine.HotelManagement;
+import HotelSearchEngine.HotelStay;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
 public class HotelManagementTest{
 
+    private MockSearchQuery number1;
+    private HotelManagement theManager;
+    private HotelStay[] mystays;
 
-    @org.junit.Before
+    private Date checkin = new Date();
+    private Date checkout = new Date(2020, 04, 12);
+
     public void setUp() throws Exception {
-
+        MockSearchQuery number1 = new MockSearchQuery("Grimbilkofinn", "Grimbilfjöll", 1, false, 1, checkin, checkout, 100 );
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
+        theManager = null;
+        number1 = null;
+        mystays = null;
 
     }
 
     @org.junit.Test
     public void testSearchResults() throws Exception {
+        assertArrayEquals(HotelStay[] mystays, theManager.searchResults(MockSearchQuery number1));
 
     }
 }
