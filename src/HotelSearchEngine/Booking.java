3c54468cb2fc57db602ad68e7ffe2391e785576f
bergerinn <and.bergsson@gmail.com>
2015-03-06 11:06:04 +0000

    Creating the classes

--- src/HotelSearchEngine/Booking.java
+++ src/HotelSearchEngine/Booking.java
@@ -1,7 +1,106 @@
 package HotelSearchEngine;
 
+import java.util.*;
+
+
 /**
  * Created by Andri on 05/03/15.
  */
+
+
+
+/**
+ *
+ */
 public class Booking {
+
+    /**
+     *
+     */
+    public Booking() {
+    }
+
+    /**
+     *
+     */
+    private void name;
+
+    /**
+     *
+     */
+    private void email;
+
+    /**
+     *
+     */
+    private void phone;
+
+    /**
+     *
+     */
+    private void roomId;
+
+    /**
+     *
+     */
+    private void hotelId;
+
+    /**
+     * @param name
+     * @param email
+     * @param phone
+     * @param roomId
+     * @param hotelId
+     * @return
+     */
+    public void InsertIntoDB(String name, String email, String phone, int roomId, String hotelId) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param name
+     * @return
+     */
+    public void setName(String name) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param email
+     * @return
+     */
+    public void setEmail(String email) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param phone
+     * @return
+     */
+    public void setPhone(String phone) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param roomid
+     * @return
+     */
+    public void setRoomId(int roomid) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param hotelid
+     * @return
+     */
+    public void setHotelId(String hotelid) {
+        // TODO implement here
+        return null;
+    }
+
 }
--- src/HotelSearchEngine/SearchQuery.java
+++ src/HotelSearchEngine/SearchQuery.java
@@ -3,5 +3,138 @@ package HotelSearchEngine;
 /**
  * Created by Andri on 05/03/15.
  */
+
+import java.util.*;
+
+/**
+ *
+ */
 public class SearchQuery {
+
+    /**
+     *
+     */
+    public SearchQuery() {
+    }
+
+    /**
+     *
+     */
+    private void hotelName;
+
+    /**
+     *
+     */
+    private void location;
+
+    /**
+     *
+     */
+    private void rating;
+
+    /**
+     *
+     */
+    private void wiFi;
+
+    /**
+     *
+     */
+    private void numberOfRooms;
+
+    /**
+     *
+     */
+    private void checkInTime;
+
+    /**
+     *
+     */
+    private void checkOutTime;
+
+    /**
+     *
+     */
+    private void totalPrice;
+
+    /**
+     * @return
+     */
+    public void search() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param name
+     * @return
+     */
+    public void setHotelName(String name) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param loc
+     * @return
+     */
+    public void setLocation(String loc) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param rating
+     * @return
+     */
+    public void setRating(int rating) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param yesOrNo
+     * @return
+     */
+    public void setWiFi(boolean yesOrNo) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param roomCount
+     * @return
+     */
+    public void setNumRooms(int roomCount) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param time
+     * @return
+     */
+    public void setCheckInTime(Date time) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param time
+     * @return
+     */
+    public void setCheckOutTime(Date time) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param price
+     * @return
+     */
+    public void setTotalPrice(int price) {
+        // TODO implement here
+        return null;
+    }
+
 }
