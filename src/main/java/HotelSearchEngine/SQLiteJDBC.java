ef0ec459b1f81add471a704aa734fc722f7981da
bergerinn <and.bergsson@gmail.com>
2015-03-29 04:59:41 +0000

    Database connected

---
+++ src/main/java/HotelSearchEngine/SQLiteJDBC.java
@@ -0,0 +1,21 @@
+package HotelSearchEngine;
+import java.sql.*;
+/**
+ * Created by Andri on 20/03/15.
+ */
+public class SQLiteJDBC {
+
+    public static void main( String args[] )
+    {
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:Hotels.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
+
